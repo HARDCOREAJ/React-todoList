98701a44a10acbfcc689f7ba4eeb8a586e003aec
HARDCOREAJ <565332342@qq.com>
2018-08-09 22:17:05 +0800

    添加多个bouldle

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,11 @@ const path = require('path');
 module.exports = {
   entry: {
     app: './src/index.js',
-    print: './src/print.js'
+    print: './src/print.js',
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  mode: 'production'
 };
