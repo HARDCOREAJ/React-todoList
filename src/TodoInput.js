49b2ab756e8db175bb8b13524ee3fd07503e2a53
HARDCOREAJ <565332342@qq.com>
2018-08-13 17:13:06 +0800

    fix.valuex2

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -9,7 +9,7 @@ export default class TodoInput extends Component {
     }
     submit(e){
         if (e.key ==='Enter'){
-            this.props.onsumibt(e)
+            this.props.onSumibt(e)
         }
     }
 
